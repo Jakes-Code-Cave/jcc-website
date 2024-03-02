3c3f0d2631764f6d0a7144acf1dfd3df52aa7b5d
jakeyboi1 <jakelandrum2004@gmail.com>
2024-03-02 16:20:15 -0600

    wording+Image

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -23,21 +23,21 @@ import navbar from '../components/navbar.vue'
                         class="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
                         Simple, easy, and effective!
                     </p>
-                    <h2 class="text-2xl font-bold">Welcome to JCC Script Studio! We provide affordable, highly customizable scripts for RedM, empowering players to create unique experiences. Our focus is on immersive, feature-packed, and modular scripts that cater to diverse playstyles. With extensive documentation and support, we ensure that players can easily customize and maximize our scripts. Join us to enhance your RedM experience and unleash your creativity!</h2>
+                    <h2 class="text-2xl font-bold">We provide affordable, highly customizable scripts for RedM, empowering players to create unique experiences. Our focus is on immersive, feature-packed, and modular scripts that cater to diverse playstyles. With extensive documentation and support, we ensure that players can easily customize and maximize our scripts. Join us to enhance your RedM experience and unleash your creativity!</h2>
                     <div class="h-6"></div>
                     <div class="grid grid-cols-2 gap-4 pt-8 border-t border-gray-800">
                         <div>
                             <p class="font-semibold text-gray-400">Made with passion, and experience</p>
                             <div class="h-4"></div>
                             <p class="font-serif text-gray-400">
-                                Here at JCC we build all of our products with a passion for RedM, and a high level of experience in the field.
+                                We build all of our products with a passion for RedM, and a high level of experience in the field.
                             </p>
                         </div>
                         <div>
                             <p class="font-semibold text-gray-400">Highly Modular</p>
                             <div class="h-2"></div>
                             <p class="font-serif text-gray-400">
-                                Here at JCC we do not believe in the highly restrictive nature of a config file, we believe in a highly modular system that allows you maximum customizability.
+                                We do not believe in the highly restrictive nature of a config file, we believe in a highly modular system that allows you maximum customizability.
                             </p>
                         </div>
                     </div>
